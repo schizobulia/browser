6f59df9a43f3c11964e0881046cb2ac843a1cf98
wujie <wujie@dc.com>
2024-05-03 22:49:57 +0800

    feat: Support for js execution during rendering

--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -10,7 +10,6 @@ use bevy::prelude::*;
 use bean::node::{ElementText, Node};
 use bean::qaq;
 use bean::ui_state::UiState;
-use component::p::PComponent;
 use css::parse_css;
 use generate::NodeResult;
 use js_engine;
@@ -38,23 +37,20 @@ pub fn render_document(
         background_color: Color::rgba(255.0, 255.0, 255.0, 1.0).into(),
         ..default()
     };
-    let mut scripts = Vec::new();
     let mut styles = Vec::new();
-    commands
-        .spawn(root)
-        .with_children(|parent: &mut ChildBuilder<'_>| {
-            let mut binding = qaq::GLOBAL_STATE.lock().unwrap();
-            let document = Html::parse_document(&html);
-            traverse_html(
-                document.root_element(),
-                parent,
-                &asset_server,
-                &mut js_runtime,
-                binding.children.as_mut(),
-                &mut scripts,
-                &mut styles,
-            );
-        });
+    
+    let document = Html::parse_document(&html);
+    let root_id = commands.spawn(root).id();
+
+    traverse_html(
+        document.root_element(),
+        &mut commands,
+        &asset_server,
+        &mut js_runtime,
+        &mut styles,
+        root_id.clone(),
+    );
+
     for style in styles {
         qaq::GLOBAL_ACTION
             .lock()
@@ -62,89 +58,78 @@ pub fn render_document(
             .actions
             .push(qaq::Action::ChangeStyleAction(parse_css(style)));
     }
-    for script in scripts {
-        let code = Box::leak(script.clone().into_boxed_str());
-        js_runtime.eval(code);
-    }
-    // js.eval("const arr = [1, 2, 3];Deno.core.ops.op_sum(arr);");
 }
 
 fn traverse_html(
     element: ElementRef,
-    commands: &mut ChildBuilder<'_>,
+    commands: &mut Commands,
     asset_server: &Res<AssetServer>,
     js_runtime: &mut js_engine::V8Runtime,
-    list: &mut Vec<Node>,
-    scripts: &mut Vec<String>,
     styles: &mut Vec<String>,
+    root_id: Entity,
 ) {
-    let tag = element.value().name().to_string();
-    let mut attributes = HashMap::new();
-    element.value().attrs.clone().iter().for_each(|attr| {
-        attributes.insert(attr.0.local.to_string(), attr.1.to_string());
-    });
-    let mut el_data: Node = Node {
-        children: Vec::new(),
-        tag_name: tag,
-        attributes: attributes,
-        text: None,
-        id: None,
-        style_sheet_list: None,
-    };
-    let res: NodeResult = generate::get_node_result(element);
-    match res {
-        // mark 大部分浏览器的逻辑是：在渲染过程中可以修改已经渲染好的dom，
-        // 但目前这里的实现(GLOBAL_STATE)存在私锁的问题。
-        NodeResult::Script(script) => {
-            // let code = Box::leak(script.clone().into_boxed_str());
-            // js_runtime.eval(code);
-            scripts.push(script);
-        }
-        NodeResult::Style(style) => {
-            styles.push(style);
-        }
-        NodeResult::Div(bundle, style, text_style) => {
-            let id = commands
-                .spawn(PComponent {})
-                .insert(bundle)
-                .with_children(|parent: &mut ChildBuilder<'_>| {
-                    for child in element.children() {
-                        if let Some(child_element) = ElementRef::wrap(child) {
-                            traverse_html(
-                                child_element.clone(),
-                                parent,
-                                asset_server,
-                                js_runtime,
-                                &mut el_data.children,
-                                scripts,
-                                styles,
-                            );
-                        } else if child.value().is_text() {
-                            let text = child.value().as_text().unwrap().to_string();
-                            if text.trim().is_empty() {
-                                continue;
-                            }
-                            let text_bundle = TextBundle::from_section(
-                                text,
-                                TextStyle {
-                                    font: asset_server.load("fonts/FiraMono-Medium.ttf"),
-                                    ..text_style
-                                },
-                            );
-                            let childern_id = parent.spawn(text_bundle).id();
-                            el_data.text = Some(ElementText {
-                                id: Some(childern_id),
-                                text: child.value().as_text().unwrap().to_string(),
-                            });
+    let mut rid = root_id.clone();
+    let mut stack = vec![element];
+    while let Some(element) = stack.pop() {
+        let res: NodeResult = generate::get_node_result(element);
+        match res {
+            NodeResult::Script(script) => {
+                js_runtime.eval(Box::leak(script.clone().into_boxed_str()));
+            }
+            NodeResult::Style(style) => {
+                styles.push(style);
+            }
+            NodeResult::Div(bundle, style, text_style) => {
+                let mut binding = qaq::GLOBAL_STATE.lock().unwrap();
+                let list: &mut Vec<Node> = binding.children.as_mut();
+                let tag = element.value().name().to_string();
+                let mut attributes = HashMap::new();
+                element.value().attrs.clone().iter().for_each(|attr| {
+                    attributes.insert(attr.0.local.to_string(), attr.1.to_string());
+                });
+                let mut el_data: Node = Node {
+                    children: Vec::new(),
+                    tag_name: tag,
+                    attributes: attributes,
+                    text: None,
+                    id: None,
+                    style_sheet_list: None,
+                };
+                // binding = qaq::GLOBAL_STATE.lock().unwrap(); 
+                let id = commands.spawn(bundle).id();
+                commands.entity(rid).push_children(&vec![id.clone()]);
+                el_data.id = Some(id.clone());
+                el_data.style_sheet_list = Some(style);
+                rid = id.clone();
+                for child in element.children().rev() {
+                    if let Some(child_element) = ElementRef::wrap(child) {
+                        stack.push(child_element.clone());
+                    } else if child.value().is_text() {
+                        let text = child.value().as_text().unwrap().to_string();
+                        if text.trim().is_empty() {
+                            continue;
                         }
+                        let text_bundle = TextBundle::from_section(
+                            text,
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                                ..text_style
+                            },
+                        );
+                        let childern_id = commands.spawn(text_bundle).id();
+                        commands.entity(id).push_children(&vec![childern_id.clone()]);
+                        el_data.text = Some(ElementText {
+                            id: Some(childern_id),
+                            text: child.value().as_text().unwrap().to_string(),
+                        });
                     }
-                })
-                .id();
-            el_data.id = Some(id);
-            el_data.style_sheet_list = Some(style);
-        }
-    };
-    list.push(el_data);
+                }
+                list.push(el_data);
+                drop(binding);
+            }
+        };
+
+    }
 }
 
 pub fn update_node_text(
